0ead79234e3c88bb444011f5d7b1c2b582000e11
piyushsehli <piyushsehli@gmail.com>
2018-07-31 11:21:44 -0400

    Bug fixes

--- Scripts/objects/meteor.ts
+++ Scripts/objects/meteor.ts
@@ -1,3 +1,29 @@
+/*
+  Source file name: meteor.ts, 
+  Author’s name: Piyush Sehli, 
+  Student Number: 300984528, 
+  Last Modified by: Piyush, 
+  Date last Modified: 30 July, 2018,
+  Program description: The goal of the game is to destroy the space ships of all 
+  the enemies by simply clicking the mouse. To maintain their 
+  life scores, star ship must avoid collision with the meteors 
+  and enemy ships. To gain score, the ship must hover over the circular ship., 
+  Revision History:
+  Third Commit: Adding sounds
+
+  @piyushsehli
+  piyushsehli committed 19 hours ago
+  
+  Second Commit: Adding an enemy ship
+
+  @piyushsehli
+  piyushsehli committed 19 hours ago
+  
+  First commit: initial stage of game.
+
+
+*/
+
 namespace objects {
     export class Meteor extends objects.GameObject {
       // member variables
